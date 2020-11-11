c6700224f2ad47d53c7a40d66122d882ccdc3391
Samuel Cornejo <sam.ach1990@gmail.com>
2020-11-11 03:46:18 -0600

    feat: Crates SectionHeader

---
+++ RavnChallenge/Views/SectionHeader.swift
@@ -0,0 +1,27 @@
+//
+//  SectionHeader.swift
+//  RavnChallenge
+//
+//  Created by Samuel Cornejo on 11/11/20.
+//
+
+import SwiftUI
+
+struct SectionHeader: View {
+    // MARK: Variables Declaration
+    let title: String
+
+    var body: some View {
+        Text(title)
+            .textStyle(.h2Default)
+            .padding(.horizontal, 16)
+            .padding(.top, 32)
+            .padding(.bottom, 8)
+    }
+}
+
+struct SectionHeader_Previews: PreviewProvider {
+    static var previews: some View {
+        SectionHeader(title: "General Information")
+    }
+}
